d7d1c93b3ed4bcf9a4c8d6c53b5810727299306d
Tasneemalabsi <tasneem.alabsi@gmail.com>
2023-03-15 12:36:02 +0300

    add new function to the main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int inputNumber = sc.nextInt();
+        System.out.println(addTwotoTheNumber(inputNumber));
+
     }
+    public static int addTwotoTheNumber(int n){
+        return n+2;
+    }
+
 }
